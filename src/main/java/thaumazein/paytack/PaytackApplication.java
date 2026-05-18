61b00398876469eced0f128dc59e957bc790e8f9
José P. <shzexyz@gmail.com>
2026-05-18 19:13:49 -0300

    chore: config `.env` and load with dotnev

--- src/main/java/thaumazein/paytack/PaytackApplication.java
+++ src/main/java/thaumazein/paytack/PaytackApplication.java
@@ -1,5 +1,6 @@
 package thaumazein.paytack;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PaytackApplication {
 
     public static void main(String[] args) {
+        Dotenv.configure()
+                .ignoreIfMissing()
+                .systemProperties()
+                .load();
+
         SpringApplication.run(PaytackApplication.class, args);
     }
 
