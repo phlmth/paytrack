60097f2a57e016eab68f76732c4243235fc0c232
José P. <shzexyz@gmail.com>
2026-05-18 19:01:21 -0300

    chore: configure postgres docker and application datasource

---
+++ src/main/java/thaumazein/paytack/PaytackApplication.java
@@ -0,0 +1,13 @@
+package thaumazein.paytack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaytackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaytackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/thaumazein/paytack/PaytackApplicationTests.java
@@ -0,0 +1,13 @@
+package thaumazein.paytack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaytackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
